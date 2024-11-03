c904d73059dc10b8099ca0ffb2ca8585f5dfb22d
extrawurst <mail@rusticorn.com>
2024-11-03 20:56:27 +0100

    docs and fixes

--- README.md
+++ README.md
@@ -4,5 +4,9 @@
 [![crates.io](https://img.shields.io/crates/v/bevy_channel_trigger)](https://crates.io/crates/bevy_channel_trigger)
 [![docs.rs](https://docs.rs/bevy_channel_trigger/badge.svg)](https://docs.rs/bevy_channel_trigger)
 
-Send events via a channels form anywhere (eg. web-dom, c-ffi) to Bevy Observers. 
+Send events via a channel from anywhere (eg. web-dom, c-ffi) to Bevy Observers. 
 Inspired by [bevy_crossbeam_event](https://github.com/johanhelsing/bevy_crossbeam_event) but using [flume](https://github.com/zesterer/flume) instead of `crossbeam` as the underlying efficient unbounded channel and delivering the events via Bevy Observers instead of `EventReader`. Furthermore we lint to be guaranteed panic free.
+
+Example uses: 
+
+* [bevy_web_drop_image_as_sprite](https://github.com/rustunit/bevy_web_drop_image_as_sprite)
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 use bevy::{ecs::event::Event, prelude::*};
 use flume::{Receiver, Sender, TryRecvError};
 
+/// channel sender to share with multiple producers and offering a simple `send` function
 #[derive(Resource, Clone, Debug)]
 pub struct ChannelSender<T: Event>(Sender<T>);
 
 impl<T: Event> ChannelSender<T> {
+    /// send `event` to our central receiver that forwards them as triggers that can be observed
     pub fn send(&self, event: impl Into<T>) {
         let event = event.into();
         if let Err(err) = self.0.send(event) {
@@ -16,7 +18,11 @@ impl<T: Event> ChannelSender<T> {
 #[derive(Resource)]
 struct EventReceiver<T: Event>(Receiver<T>);
 
+/// Extension to Bevy `App` that allows ergonomic creation of the channel
 pub trait ChannelTriggerApp {
+    /// Spawns a channel registers the receiver as a resource and returns the `ChannelSender<T>`
+    /// This sender can be used from anywhere to send events into the Bevy world.
+    /// These triggers can be subscribed to via `app.observe`.
     fn add_channel_trigger<T: Event>(&mut self) -> ChannelSender<T>;
 }
 
